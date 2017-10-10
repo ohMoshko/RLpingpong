e7191f3ea2ce21963611af948f49ec8caa3ab199
Ohad Moshkovitz <ohadmosh@gmail.com>
2017-10-10 13:44:01 +0300

    added parameters to player 2. added a new game: no random start.

--- qlearn.py
+++ qlearn.py
@@ -34,7 +34,6 @@ import shutil
 from keras.utils.visualize_util import plot
 from aenum import Enum
 
-
 IMAGE_WIDTH = 80
 IMAGE_HEIGHT = 80
 IMAGE_NUM_OF_CHANNELS = 4
@@ -52,7 +51,7 @@ BATCH = 32  # size of one minibatch
 FRAME_PER_ACTION = 1
 
 test_sequential_command = "KERAS_BACKEND=theano THEANO_FLAGS=floatX=float32,device=gpu,force_device=True," \
-                   "cuda.root=/usr/local/cuda,lib.cnmem=0.2 python ./test_sequential_training.py "
+                          "cuda.root=/usr/local/cuda,lib.cnmem=0.2 python ./test_sequential_training.py "
 
 
 class CurrentPlayer(Enum):
@@ -70,35 +69,38 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copy2(s, d)
 
 
-def train_sequentially(left_player, right_player):
+def train_sequentially(left_player, right_player, first_learning_player):
     # moving old trials to old_trials folder
     if os.path.exists('trials_sequentially'):
         copytree('trails_sequentially', 'old_trials_sequentially')
         shutil.rmtree('trails_sequentially')
     os.mkdir('trials_sequentially', 0755)
 
-    current_training_player = CurrentPlayer.left
-
-    left_player.num_of_trains = left_player.num_of_trains + 1
+    if (first_learning_player == CurrentPlayer.left):
+        left_player.num_of_trains = left_player.num_of_trains + 1
+    elif (first_learning_player == CurrentPlayer.right):
+        right_player.num_of_trains = right_player.num_of_trains + 1
 
     # open up a game state to communicate with emulator
     game_state = game.GameState()
 
     # store the previous observations in replay memory
-    D = deque()
+    D1 = deque()
+    D2 = deque()
 
     # get the first state (do nothing) and pre-process the image to 4x80x80
     do_nothing = np.zeros(NUM_OF_ACTIONS)
     do_nothing[0] = 1
 
     single_game_frame, r_0, terminal, _, _ = game_state.frame_step(do_nothing, do_nothing)
-    single_game_frame_gray = skimage.color.rgb2gray(single_game_frame)
-    single_game_frame_gray = skimage.transform.resize(single_game_frame_gray, (IMAGE_WIDTH, IMAGE_HEIGHT))
-    single_game_frame_gray = skimage.exposure.rescale_intensity(single_game_frame_gray, out_range=(0, 255))
 
-    # stacking 4 images together to form a state
-    current_state = np.stack((single_game_frame_gray, single_game_frame_gray, single_game_frame_gray,
-                                      single_game_frame_gray), axis=0)
+    single_game_frame_grey = skimage.color.rgb2gray(single_game_frame)
+    single_game_frame_grey = skimage.transform.resize(single_game_frame_grey, (IMAGE_WIDTH, IMAGE_HEIGHT))
+    single_game_frame_grey = skimage.exposure.rescale_intensity(single_game_frame_grey, out_range=(0, 255))
+
+    # stacking 4 images together to form a state: 4 images = state
+    current_state = np.stack((single_game_frame_grey, single_game_frame_grey, single_game_frame_grey,
+                              single_game_frame_grey), axis=0)
 
     current_state = current_state.reshape(1, current_state.shape[0],
                                           current_state.shape[1],
@@ -112,64 +114,89 @@ def train_sequentially(left_player, right_player):
     while True:
         loss = 0
         Q_sa = 0
-        action_index1 = 0
-        reward = 0
-        Q_sb = 0
-        action_index2 = 0
 
+        # player1
+        action_index1 = 0
+        reward1 = 0
         action_left_player = np.zeros([NUM_OF_ACTIONS])
+
+        # player2
+        action_index2 = 0
+        reward2 = 0
         action_right_player = np.zeros([NUM_OF_ACTIONS])
 
         # choose an action epsilon greedy
         if observation_counter % FRAME_PER_ACTION == 0:
-            q = left_player.model.predict(current_state)  # input a stack of 4 images, get the prediction
-            max_Q = np.argmax(q)
-            action_index1 = max_Q
-            action_left_player[action_index1] = 1
+            if (current_training_player == CurrentPlayer.left):
+                if (random.random() <= epsilon):
+                    action_index1 = random.randrange(NUM_OF_ACTIONS)
+                    action_left_player[action_index1] = 1
+                else:
+                    q = left_player.model.predict(current_state)  # input a stack of 4 images, get the prediction
+                    max_Q = np.argmax(q)
+                    action_index1 = max_Q
+                    action_left_player[action_index1] = 1
 
-            if random.random() <= epsilon:  # for left player
-                action_index2 = random.randrange(NUM_OF_ACTIONS)
-                action_right_player[action_index2] = 1
-
-            else:
+                # right player:
                 q = right_player.model.predict(current_state)  # input a stack of 4 images, get the prediction
                 max_Q = np.argmax(q)
                 action_index2 = max_Q
                 action_right_player[action_index2] = 1
 
+            elif (current_training_player == CurrentPlayer.right):
+                if random.random() <= epsilon:
+                    action_index2 = random.randrange(NUM_OF_ACTIONS)
+                    action_right_player[action_index2] = 1
+
+                else:
+                    q = right_player.model.predict(current_state)  # input a stack of 4 images, get the prediction
+                    max_Q = np.argmax(q)
+                    action_index2 = max_Q
+                    action_right_player[action_index2] = 1
+
+                # left player:
+                q = left_player.model.predict(current_state)  # input a stack of 4 images, get the prediction
+                max_Q = np.argmax(q)
+                action_index1 = max_Q
+                action_left_player[action_index1] = 1
+
         # we reduced the epsilon gradually
         if (epsilon > FINAL_EPSILON) and (observation_counter > OBSERVATION):
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
-        # run the selected action and observed next state and reward
-        if current_training_player == CurrentPlayer.left:
-            single_game_frame_colored, reward, terminal, score, _ = \
-                game_state.frame_step(action_right_player, action_left_player)
+        single_game_frame_colored, reward1, terminal, score, _ = \
+            game_state.frame_step(action_left_player, action_right_player)
 
-        elif current_training_player == CurrentPlayer.right:
-            single_game_frame_colored, reward, terminal, score, _ = \
-                game_state.frame_step(action_left_player, action_right_player)
-            reward = -reward
+        if current_training_player == CurrentPlayer.right:
+            reward2 = -reward1
 
-        single_game_frame_gray = skimage.color.rgb2gray(single_game_frame_colored)
-        thresh = threshold_otsu(single_game_frame_gray)
-        single_game_frame_binary = single_game_frame_gray > thresh
+        single_game_frame_grey = skimage.color.rgb2gray(single_game_frame_colored)
+        thresh = threshold_otsu(single_game_frame_grey)
+        single_game_frame_binary = single_game_frame_grey > thresh
 
         single_game_frame_binary = skimage.transform.resize(single_game_frame_binary, (80, 80))
         single_game_frame_binary = skimage.exposure.rescale_intensity(single_game_frame_binary, out_range=(0, 255))
         single_game_frame_binary = single_game_frame_binary.reshape(1, 1, single_game_frame_binary.shape[0],
                                                                     single_game_frame_binary.shape[1])
-
+        # next 4 images
         next_state = np.append(single_game_frame_binary, current_state[:, :3, :, :], axis=1)
 
-        D.append((current_state, action_index2, reward, next_state, terminal))
-        if len(D) > REPLAY_MEMORY:
-            D.popleft()
+        if (current_training_player == CurrentPlayer.left):
+            D1.append((current_state, action_index1, reward1, next_state, terminal))
+            if len(D1) > REPLAY_MEMORY:
+                D1.popleft()
+        elif (current_training_player == CurrentPlayer.right):
+            D2.append((current_state, action_index2, reward2, next_state, terminal))
+            if len(D2) > REPLAY_MEMORY:
+                D2.popleft()
 
         # only train if done observing
         if observation_counter > OBSERVATION:
             # sample a minibatch to train on
-            minibatch = random.sample(D, BATCH)
+            if current_training_player == CurrentPlayer.left:
+                minibatch = random.sample(D1, BATCH)
+            elif current_training_player == CurrentPlayer.right:
+                minibatch = random.sample(D2, BATCH)
 
             inputs = np.zeros((BATCH, next_state.shape[1], next_state.shape[2],
                                next_state.shape[3]))  # 32, 4, 80, 80
@@ -182,19 +209,25 @@ def train_sequentially(left_player, right_player):
                 reward_t = minibatch[i][2]
                 next_state_t = minibatch[i][3]
                 terminal_t = minibatch[i][4]
-                # if terminated, only equals reward
 
                 inputs[i:i + 1] = current_state_t
 
-                targets[i] = right_player.model.predict(current_state_t)
-                Q_sa = right_player.model.predict(next_state_t)
+                if (current_training_player == CurrentPlayer.left):
+                    targets[i] = left_player.model.predict(current_state_t)
+                    Q_sa = left_player.model.predict(next_state_t)
+                elif (current_training_player == CurrentPlayer.right):
+                    targets[i] = right_player.model.predict(current_state_t)
+                    Q_sa = right_player.model.predict(next_state_t)
 
                 if terminal_t:
                     targets[i, action_t] = reward_t
                 else:
                     targets[i, action_t] = reward_t + GAMMA * np.max(Q_sa)
 
-            loss += right_player.model.train_on_batch(inputs, targets)
+            if (current_training_player == CurrentPlayer.left):
+                loss += left_player.model.train_on_batch(inputs, targets)
+            elif (current_training_player == CurrentPlayer.right):
+                loss += right_player.model.train_on_batch(inputs, targets)
 
         current_state = next_state
         observation_counter = observation_counter + 1
@@ -212,9 +245,9 @@ def train_sequentially(left_player, right_player):
                     json.dump(right_player.model.to_json(), outfile)
 
         current_time = datetime.datetime.now()
-        elapsedTime = (current_time - start_time).total_seconds()
+        elapsed_time = (current_time - start_time).total_seconds()
 
-        if elapsedTime >= 30 * 60:
+        if elapsed_time >= 30 * 60:
             num_folder += 1
             start_time = datetime.datetime.now()
 
@@ -232,7 +265,7 @@ def train_sequentially(left_player, right_player):
                 shutil.copy2('model2.h5', 'trials/' + 'right_player_learning' +
                              str(right_player.num_of_trains) + '/' + str(num_folder) + '/model2.h5')
 
-        if terminal: # game over
+        if terminal:  # game over
             if score[0] < score[1]:
                 right_player.num_of_wins_in_a_row += 1
                 left_player.num_of_wins_in_a_row = 0
@@ -279,15 +312,21 @@ def play_game(args):
         right_player = Player('model2.h5')
         right_player.build_model()
 
+        if (int(args['first_learning_player']) == 1):
+            first_learning_player = CurrentPlayer.left
+        elif (int(args['first_learning_player']) == 2):
+            first_learning_player = CurrentPlayer.right
+
         if args['learning_mode'] == 'SEQUENTIALLY':  # one after another (left and then right and then left...)
-            train_sequentially(left_player, right_player)
-        # elif (args['learning_mode'] == 'SIMULTANEOUSLY'):
-        #     train_simultaneously(left_player, right_player)
+            train_sequentially(left_player, right_player, first_learning_player)
+            # elif (args['learning_mode'] == 'SIMULTANEOUSLY'):
+            #     train_simultaneously(left_player, right_player)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Ping-Pong Q-Learning')
-    parser.add_argument('-l', '--learning_mode', help='SEQUENTIALLY, SIMULTANEOUSLY', required=False)
+    parser.add_argument('-l', '--learning_mode', help='SEQUENTIALLY, SIMULTANEOUSLY', required=True)
+    parser.add_argument('-p', '--first_learning_player', help='1, 2', required=False)
     args = vars(parser.parse_args())
     play_game(args)
 
